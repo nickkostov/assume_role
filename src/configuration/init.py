7b0dae76892e0bc3a060b998c84a685925a56fb5
nickkostov <nikolay.yavorov.kostov@gmail.com>
2023-01-03 19:18:03 +0200

    refactoring due to click

--- src/configuration/init.py
+++ src/configuration/init.py
@@ -6,18 +6,19 @@ import os
 file_names = ['roles', 'mfa_names', 'mfa_account_ids']
 roles = input('Provide a list of your roles separated by coma: ')
 mfa_names = input('Provide a list of your MFA devices serparated by coma: ')
-mfa_account_ids = input('Provide a list of your MFA devices serparated by coma: ')
+mfa_account_ids = input('Provide a list of your MFA device IDs: ')
 
 contents = [ roles, mfa_names, mfa_account_ids]
 
 
 
+
 def create_config_dir():
-  if os.path.exists(os.environ['HOME'] + '/.assume_configuration_folder/'):
+  if os.path.exists(os.environ['HOME'] + '/.assume_config/'):
     print("Directory already exists.")
   else:
     try:
-      os.mkdir(os.environ['HOME'] + '/.assume_configuration_folder/')
+      os.mkdir(os.environ['HOME'] + '/.assume_config/')
     except PermissionError:
       print("Permission denied: Unable to create directory at the specified location.")
 create_config_dir()
@@ -26,9 +27,10 @@ def create_configuration_files():
     
     for file_name in file_names:
     # Creating the files:
-      with open(os.environ['HOME'] + '/.assume_configuration_folder/' + file_name, 'w') as f:
+      with open(os.environ['HOME'] + '/.assume_config/' + file_name, 'w') as f:
         for i, file_name in enumerate(file_names):
         # Writing what we want:
-            with open(os.environ['HOME'] + '/.assume_configuration_folder/' + file_name, 'w') as f:
-                f.write(contents[i])
+            with open(os.environ['HOME'] + '/.assume_config/' + file_name, 'w') as f:
+                f.write(contents[i].replace(' ', '').replace(',', '\n'))
+
 create_configuration_files()
