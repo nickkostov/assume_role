c01df5bfe9b4898daf5e354151d9eb2b6ed71e04
Nikolay Kostov (TME) <nikolay.kostov@external.toyota-europe.com>
2023-03-12 03:30:48 +0200

    configuration reader

---
+++ assume_tool/configuration.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import os
+
+def print_profiles ():
+    with open(os.environ['HOME'] + '/.aws/credentials', 'r') as f:
+        contents = f.read()
+    
+    string_list = contents.split('\n')
+    for line in string_list:
+       if line.startswith("[") and line.endswith("]"):
+          res = line[1:-1]
+          print(res)
+
+def readroles(fpath):
+    with open(fpath, 'r') as f:
+        contents = f.read()
+    string_list = contents.split()
+    output = '\n'.join(string_list)
+    print('Available roles are: \n' + output)
+
+def mfa_read_names(fpath):
+    with open(fpath, 'r') as f:
+        contents = f.read()
+    string_list = contents.split()
+    output = '\n'.join(string_list)
+    print('Your MFA Device Names are: \n' + output)
+
+def mfa_acc_id(fpath):
+    with open(fpath, 'r') as f:
+        contents = f.read()
+    string_list = contents.split()
+    output = '\n'.join(string_list)
+    print('Your MFA Account IDs are: \n' + output)
